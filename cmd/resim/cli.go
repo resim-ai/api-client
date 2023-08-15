4b2b713bfe6c33078730e51dff38dd5f361ac6fe
Austin McDonald <austin@resim.ai>
2023-08-15 17:22:02 -0400

    add replacer for envvars

--- cmd/resim/cli.go
+++ cmd/resim/cli.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"strings"
 
 	"github.com/resim-ai/api-client/cmd/resim/commands"
 	"github.com/spf13/viper"
@@ -12,6 +13,7 @@ const EnvPrefix = "RESIM"
 func main() {
 	viper.SetEnvPrefix(EnvPrefix)
 	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 	err := commands.Execute()
 	if err != nil && err.Error() != "" {
 		log.Fatal(err)

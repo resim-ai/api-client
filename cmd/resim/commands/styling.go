d84fa85ee6ab4ed884ddb790fee34e3a1c255fe6
SimonBox <simon@resim.ai>
2023-08-29 11:42:48 -0700

    Getting Styley (#44)
    This PR sets us up to be able customize the style of help, usage and error 
    messages. The effect of the PR is *almost* a noop on current behavior. It
    makes a very small change to the styling of section headings in usage
    messages (see images below), in order to demonstrate the functionality. 
    
    The main aim of the PR is to set us up with the ability to make greater 
    customization in the future. For example, the next PR (#45 ) will aim to 
    separate Flags into required and optional sections per a customer request.
    
    Old:
    ![image](https://github.com/resim-ai/api-client/assets/1348072/4714d634-1729-40d6-9e84-937f7f17a0bf)
    New:
    ![image](https://github.com/resim-ai/api-client/assets/1348072/a550212a-fcac-47b8-8cc6-4d0f7941e9b0)
    
    [Asana task](https://app.asana.com/0/1205228215063249/1205227572053881/f)
    ## Guide to reproduce test results
    run any `resim` command with `--help` and verify the new heading styling.
    
    ## Checklist
    
    - [x] I have self-reviewed this change.
    - [x] I have tested this change.
    - [ ] This change is covered by tests that are already landed, or in this PR.

---
+++ cmd/resim/commands/styling.go
@@ -0,0 +1,73 @@
+// Enables applying custom ReSim styling to help and usage text.
+//
+// Cobra provides two methods for doing this:
+// 1. Using cobra.Command.SetUsageFunc() allows an arbitrary function to be
+// provided to produce the command usage help text. This is powerful, but
+// also requires the author of the function to implement all functionality.
+// 2. Using cobra.Command.SetUsageTemplate() allows the existing text template
+// for usage to be modified or replaced. This gives the author control over the
+// contents of usage message and takes advantage of all the built in cobra
+// functionality for generating the data in the text template. Further, text
+// templates are quite powerful because they can be used to execute arbitrary
+// functions, including user defined functions. That may be added using:
+// cobra.AddTemplateFunc("key", UserFunction). Therefore we follow this approach
+// here in defining ReSim's custom styling.
+//
+// In this file, first the ReSimUsageTemplate is defined. Below this any
+// custom template functions are defined. Finally a styling helper:
+// ApplyReSimStyle is defined. Style templates are inherited by child commands
+// so this function need only be applied once to the root command (rootCmd).
+
+package commands
+
+import (
+	"text/template"
+
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
+
+var ReSimUsageTemplate string = `{{StyleHeading "USAGE"}}{{if .Runnable}}
+  {{.UseLine}}{{end}}{{if .HasAvailableSubCommands}}
+  {{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
+
+{{StyleHeading "ALIASES"}}
+  {{.NameAndAliases}}{{end}}{{if .HasExample}}
+
+{{StyleHeading "EXAMPLES"}}
+{{.Example}}{{end}}{{if .HasAvailableSubCommands}}{{$cmds := .Commands}}{{if eq (len .Groups) 0}}
+
+{{StyleHeading "AVAILABLE COMMANDS"}}{{range $cmds}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{else}}{{range $group := .Groups}}
+
+{{.Title}}{{range $cmds}}{{if (and (eq .GroupID $group.ID) (or .IsAvailableCommand (eq .Name "help")))}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if not .AllChildCommandsHaveGroup}}
+
+{{StyleHeading "ADDITIONAL COMMANDS"}}{{range $cmds}}{{if (and (eq .GroupID "") (or .IsAvailableCommand (eq .Name "help")))}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
+
+{{StyleHeading "FLAGS"}}
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
+
+{{StyleHeading "GLOBAL FLAGS"}}
+{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
+
+{{StyleHeading "ADDITIONAL HELP TOPICS"}}
+{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
+  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+
+Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
+`
+
+var templateFuncs = template.FuncMap{
+	"StyleHeading": styleHeading,
+}
+
+func styleHeading(s string) string {
+	return color.New(color.Bold).SprintFunc()(s)
+}
+
+func ApplyReSimStyle(cmd *cobra.Command) {
+	cobra.AddTemplateFuncs(templateFuncs)
+	cmd.SetUsageTemplate(ReSimUsageTemplate)
+}

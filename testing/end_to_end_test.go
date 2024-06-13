480763a482d71a46130c2db715a790af49eb9832
Iain Whiteside <iain@resim.ai>
2024-06-13 09:34:07 +0000

    blah

--- testing/end_to_end_test.go
+++ testing/end_to_end_test.go
@@ -2676,10 +2676,10 @@ func (s *EndToEndTestSuite) TestBatchAndLogs() {
 	var logs []api.JobLog
 	err = json.Unmarshal([]byte(output.StdOut), &logs)
 	s.NoError(err)
-	s.Len(logs, 6)
+	s.Len(logs, 7)
 	for _, log := range logs {
 		s.Equal(jobID2, *log.JobID)
-		s.Contains([]string{logName, logName2, "experience-worker.log", "metrics-worker.log", "experience-container.log", "metrics-container.log"}, *log.FileName)
+		s.Contains([]string{logName, logName2, "experience-worker.log", "metrics-worker.log", "experience-container.log", "metrics-container.log", "metrics.binproto"}, *log.FileName)
 	}
 
 	// Pass blank name / id to logs:
